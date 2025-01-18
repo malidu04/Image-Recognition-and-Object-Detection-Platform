94a68dc8d1a7da063f3b97e167c6ce81eb02996a
Malidu Pahasara <malidupahasara04@gmail.com>
2025-01-19 00:19:54 +0530

    message

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    
+})
